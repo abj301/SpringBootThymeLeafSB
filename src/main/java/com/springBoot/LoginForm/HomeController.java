51b5481e49f4baeb701646d0cda86141619c696d
Abhishek Jambure <abhishek.jambure@morningstar.com>
2020-09-21 00:03:46 +0530

    added login form, made connection to Mysql

--- src/main/java/com/springBoot/LoginForm/HomeController.java
+++ src/main/java/com/springBoot/LoginForm/HomeController.java
@@ -2,11 +2,14 @@ package com.springBoot.LoginForm;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	@RequestMapping("home")
-	public String showPage() {
-		return "home";
+	public ModelAndView showPage(CredentialInformation getCredentials, ModelAndView mv) {
+		mv.addObject("CredentialInformationObj", getCredentials);
+		mv.setViewName("home");
+		return mv;
 	}
 }
--- src/main/java/com/springBoot/LoginForm/LoginFormApplication.java
+++ src/main/java/com/springBoot/LoginForm/LoginFormApplication.java
@@ -2,7 +2,9 @@ package com.springBoot.LoginForm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("com.springBoot.LoginForm")
 @SpringBootApplication
 public class LoginFormApplication {
 
